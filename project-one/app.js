da6429f6aa479e3a6583f53f71aec7a4030dad07
YadNajmaldin <Yad.sarkawt@hotmail.com>
2020-06-23 09:03:31 -0500

    update readme.me

--- project-one/app.js
+++ project-one/app.js
@@ -1,6 +1,7 @@
-//https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=Cheddar%20Cheese
-
+// The FoodData Central API provides access to FoodData Central (FDC)
 const baseURL = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=wyCbR5lOQSmtnWopRJweC3FIHDJbnH1NrjFKZaas'
+
+
 
 const queryURL = baseURL +"&query=" + name; 
 
@@ -8,28 +9,30 @@ const getData = () => {
     $.ajax({
       method: 'GET' ,
       url: queryURL,
-
     }).done((data)=>{
         for (let i=0 ; i <data.foods.length; i++){
            
            for(let j=0 ; j < data.foods[i].foodNutrients.length ; j++){
-                 let divI = $(`<div> -  ${data.foods[i].foodNutrients[j].nutrientName} : ${data.foods[i].foodNutrients[j].nutrientNumber}</div> `)
-                 $('.info').append(divI);
+             let divI = $(`<div> -  ${data.foods[i].foodNutrients[j].nutrientName} : ${data.foods[i].foodNutrients[j].nutrientNumber}</div> `)
+             $('.info').append(divI);
                
            }
         
-            }
-        
-            }) 
         }
+        
+    }) 
+}
        
-  
+
+
+
+  // getting data after submitiing input by EventListener
   $(()=> {
     $('form').on('submit', (event) => {
         event.preventDefault()
         name = $('input[type="text"]').val()
         getData()
+// clearing input after submit
+        $('input[type="text"]').val('')
     })
   });
-
-// empty out the inpu
--- project-one/readme.md
+++ project-one/readme.md
@@ -1,11 +1,9 @@
-#
+# Nutrition Calculator
 
 
-##what does it do
-
-
-
+## Nutrition Calculator allows you to view nutritional facts. Search through thousands of food items including food from popular restaurants and store brands by using The FoodData Central API that provides access to FoodData Central (FDC).
 
 
 
-###asdasdasd 
+
+
