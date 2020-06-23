3b07e6328f9659363ca39b976082b5d8bdf3eaa4
YadNajmaldin <Yad.sarkawt@hotmail.com>
2020-06-23 10:01:59 -0500

    updates js

--- project-one/app.js
+++ project-one/app.js
@@ -3,34 +3,32 @@ const baseURL = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=wyCbR5lOQS
 
 
 
-const queryURL = baseURL +"&query=" + name; 
+let queryURL = baseURL +"&query=" + name; 
 
 const getData = () => {
     $.ajax({
-      method: 'GET' ,
-      url: queryURL,
+    method: 'GET' ,
+    url: queryURL,
     }).done((data)=>{
         for (let i=0 ; i <data.foods.length; i++){
-           
-           for(let j=0 ; j < data.foods[i].foodNutrients.length ; j++){
-             let divI = $(`<div> -  ${data.foods[i].foodNutrients[j].nutrientName} : ${data.foods[i].foodNutrients[j].nutrientNumber}</div> `)
-             $('.info').append(divI);
-               
-           }
+            for(let j=0 ; j < data.foods[i].foodNutrients.length ; j++){
+            let divI = $(`<div> -  ${data.foods[i].foodNutrients[j].nutrientName} : ${data.foods[i].foodNutrients[j].nutrientNumber}</div> `)
+            $('.info').append(divI);
+            }
         
         }
         
     }) 
 }
-       
-
-
 
   // getting data after submitiing input by EventListener
-  $(()=> {
+$(()=> {
     $('form').on('submit', (event) => {
+        
         event.preventDefault()
         name = $('input[type="text"]').val()
+        queryURL = baseURL +"&query=" + name; 
+        console.log(queryURL);
         getData()
 // clearing input after submit
         $('input[type="text"]').val('')
